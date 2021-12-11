c03527981871ca203a65072496798bb60e6c30c6
kkstarkk <87898472+kkstarkk@users.noreply.github.com>
2021-12-11 10:26:34 +0000

    Create Grading_Student.py

---
+++ week_1/Grading_Student.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    for i in grades:
+        num=math.ceil(i/5) * 5
+        if num - i < 3 and i < 38:
+            continue
+        elif num - i < 3:
+            grades[grades.index(i)]=num
+    return grades       
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
