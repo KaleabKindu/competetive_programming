af584c9efd1a6561420745e7ba0a12f6e70e9678
kkstarkk <87898472+kkstarkk@users.noreply.github.com>
2021-12-13 10:03:02 +0000

    Update selectionsort.py

--- week_1/selectionsort.py
+++ week_1/selectionsort.py
@@ -2,7 +2,7 @@ class Solution:
     def selectionSort(self, arr,n):
         for i in range(n-1):
             min=i+1
-            for j in range(i+1,n):
+            for j in range(i+2,n):
                 if arr[j] < arr[min]:
                     min=j
             if arr[min] < arr[i]:
