5a7bf8af699897abb4e2172b7bb371b9c433c25c
kkstarkk <87898472+kkstarkk@users.noreply.github.com>
2022-02-26 15:27:18 -0300

    Time: 52 ms (75.74%), Space: 14.1 MB (65.76%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.py
@@ -1,6 +1,7 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         stak=set()
+        nums2 = set(nums2)
         for i in nums1:
             if i in nums2:
                 stak.add(i)
