225edb1b7f48d26b3ebdd61323a4480fd54d7d95
kkstarkk <87898472+kkstarkk@users.noreply.github.com>
2022-03-09 11:59:20 -0300

    Time: 45 ms (59.25%), Space: 13.9 MB (42.33%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,8 +1,8 @@
 class Solution:
-    # @lru_cache()
-    def fib(self, n: int, memo = {0:0, 1:1}) -> int:
-        if n in memo:
-            return memo[n]
-        memo[n] = self.fib(n - 2) + self.fib(n - 1)
-        return memo[n]
+    @lru_cache()
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return 0 if n == 0 else 1
+        else:
+            return self.fib(n - 2) + self.fib(n - 1)
         
