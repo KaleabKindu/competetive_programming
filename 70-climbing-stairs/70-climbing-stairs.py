0b0c4e934eb50da863ddc903477d07d6c1d155cb
kkstarkk <87898472+kkstarkk@users.noreply.github.com>
2022-03-10 11:25:02 -0300

    Time: 24 ms (98.31%), Space: 13.9 MB (75.33%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,12 +1,10 @@
 class Solution:
-    def climbStairs(self, n: int, memo = {}) -> int:
-        if n not in memo:
-            if n < 2 :
-                memo[n] = 1 
-            else:
-                memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
+    @lru_cache()
+    def climbStairs(self, n: int) -> int:
+        if n < 2 :
+            return 1 
 
-        return memo[n]
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
         
             
             
