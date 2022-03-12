ffcb5a6ab177892a133583c9ea020b2aea09d833
kkstarkk <87898472+kkstarkk@users.noreply.github.com>
2022-03-12 10:50:19 -0300

    Time: 51 ms (36.55%), Space: 14 MB (32.41%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,19 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        
+        
         @lru_cache()
-        def helper(index):
-            if index==len(nums)-1:
+        def dfs(index):
+            if index > len(nums) - 1:
+                return 0
+            if index == len(nums) - 1:
                 return nums[index]
-            if index==len(nums)-2:
-                return max(nums[index],nums[index+1])
-            one=helper(index+1)
-            two=helper(index+2)+nums[index]
-            return max(one, two)  
-        
-        
-        return helper(0)
-       
             
+            steal = nums[index] + dfs(index + 2)
             
+            Nsteal = dfs(index + 1)
+            
+            return  max(steal, Nsteal)
         
-        
+        return dfs(0)
