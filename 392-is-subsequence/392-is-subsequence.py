5bfe93ca0757dec00ba77322691d6769a8656c15
kkstarkk <87898472+kkstarkk@users.noreply.github.com>
2022-03-16 11:26:53 -0300

    Time: 55 ms (29.55%), Space: 27.6 MB (5.21%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        
+        @lru_cache()
+        def dp(i, j):
+            if i >= len(s):
+                return True
+            if j >= len(t) and i < len(s):
+                return False
+            if s[i] == t[j]:
+                return dp(i + 1, j + 1)
+            else:
+                return dp(i, j + 1)
+        
+        return dp(0, 0)
+                
