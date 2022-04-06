5cabd9606fe678d7144546e7086dd703ba35c7ff
kkstarkk <87898472+kkstarkk@users.noreply.github.com>
2022-04-06 19:04:11 +0300

    Time: 183 ms (25.25%), Space: 15.6 MB (54.36%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,24 +1,38 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        graph = defaultdict(list)
-        for course, prerequisite in prerequisites:
-            graph[course].append(prerequisite)
-        stak = []
-        self.cycle = False
-        visited = set()
-        path = set()
-        def dfs(crse):
-            if crse in path:
-                return True
-            path.add(crse)
-            for neighbor in graph[crse]:
-                if neighbor not in visited:
-                    if dfs(neighbor): return True
-            stak.append(crse)
-            path.remove(crse)
-            visited.add(crse)
-            return False
+        graph = defaultdict(set)
+        degree = {x: 0 for x in range(numCourses)}
+        for course_1, course_2 in prerequisites:
+            graph[course_1].add(course_2)
+            degree[course_2] += 1
+        queue = deque()
         for i in range(numCourses):
-            if i not in visited:
-                if dfs(i): return []
-        return stak 
+            if degree[i] == 0:
+                queue.append(i)
+        top_sort = []
+        while queue:
+            cur = queue.popleft()
+            top_sort.append(cur)
+            for neighbor in graph[cur]:
+                degree[neighbor] -= 1
+                if degree[neighbor] == 0:
+                    queue.append(neighbor)
+                    
+        return top_sort[::-1] if len(top_sort) == numCourses else []
+            
+            
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
