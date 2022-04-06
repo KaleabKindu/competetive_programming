ce9377441247e91d016d02b3a162b54919e86edd
kkstarkk <87898472+kkstarkk@users.noreply.github.com>
2022-04-06 18:34:42 +0300

    Time: 134 ms (56.48%), Space: 15.5 MB (74.31%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,19 +1,31 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = defaultdict(list)
+        graph = defaultdict(set)
+        degree = {x: 0 for x in range(numCourses)}
         for course_1, course_2 in prerequisites:
-            graph[course_1].append(course_2)
-        path = set()
-        def dfs(crse):
-            if crse in path:
-                return False
-            path.add(crse)
-            for course in graph[crse]:
-                if not dfs(course): return False
-            graph[crse] = []
-            path.remove(crse)
-            return True
-         
+            graph[course_1].add(course_2)
+            degree[course_2] += 1
+        queue = deque()
         for i in range(numCourses):
-               if not dfs(i): return False
-        return True
+            if degree[i] == 0:
+                queue.append(i)
+        top_sort = 0
+        while queue:
+            cur = queue.popleft()
+            top_sort += 1
+            for neighbor in graph[cur]:
+                degree[neighbor] -= 1
+                if degree[neighbor] == 0:
+                    queue.append(neighbor)
+                    
+        return True if top_sort == numCourses else False
+            
+            
+    
+        
+        
+            
+            
+            
+       
+            
