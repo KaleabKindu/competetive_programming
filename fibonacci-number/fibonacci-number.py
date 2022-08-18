611d72496733cde44844a87a077ff1e36d5b864c
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-08-18 14:12:55 +0300

    Time: 24 ms, Memory: 13.9 MB - LeetHub

---
+++ fibonacci-number/fibonacci-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        @lru_cache(None)
+        def fib(n):
+            if n <= 1:
+                return n
+            return fib(n - 1) + fib(n - 2)
+        return fib(n)
