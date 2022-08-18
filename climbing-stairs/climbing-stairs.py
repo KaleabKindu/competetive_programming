d267468bf9ad255161d1f1873a08772fc4ab47fe
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-08-18 15:04:55 +0300

    Time: 52 ms, Memory: 13.8 MB - LeetHub

---
+++ climbing-stairs/climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    @lru_cache()
+    def climbStairs(self, n: int) -> int:
+        if n < 2 :
+            return 1 
+
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+        
+            
+            
