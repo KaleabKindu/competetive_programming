1aa66a67c763b1fb108b7cd074f86adae33920ab
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-09-22 14:22:50 +0300

    Time: 56 ms (94.57%), Space: 13.9 MB (29.61%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.py
@@ -3,6 +3,6 @@ class Solution:
         n = len(words)
         answer = [words[0]]
         for i in range(n):
-            if Counter(words[i]) != Counter(answer[-1]):
+            if sorted(words[i]) != sorted(answer[-1]):
                 answer.append(words[i])
         return answer
