1ff2c00688bdf715e0a21e4a9563493b75715df6
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-09-23 21:19:16 +0300

    Time: 883 ms (5.00%), Space: 14.2 MB (11.65%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        answer = set()
+        n = len(candidates)
+        path = []
+        def helper(i = 0):
+            if i >= n:
+                return 
+            if sum(path) >= target:
+                if sum(path) == target:
+                    answer.add(tuple([*path]))
+                return 
+            helper(i + 1)
+            path.append(candidates[i])
+            helper(i)
+            helper(i + 1)
+            path.pop()
+        helper()
+        return [list(temp) for temp in answer]
