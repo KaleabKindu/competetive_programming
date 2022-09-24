a3522714a9efe140d65bac0ef7262b17f0891bdd
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-09-24 22:11:59 +0300

    Time: 218 ms (19.92%), Space: 13.8 MB (99.01%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,37 +1,20 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         # holds the numbers in each row
-        rows = defaultdict(lambda: defaultdict(int))
+        rows = defaultdict(set)
         # holds the numbers in each column
-        cols = defaultdict(lambda: defaultdict(int))
+        cols = defaultdict(set)
         # holds numbers in each 3x3 boxes
-        boxes = defaultdict(lambda: defaultdict(int))
-        candidates = [str(temp) for temp in range(1, 10)]
-        # holds key value pair of an index to a the corresponding box
-        box = {}
+        boxes = defaultdict(set)
         
-        # add the numbers to thier respective rows and cols
-        for i in range(len(board)):
-            for j in range(len(board[0])):
+        for i in range(9):
+            for j in range(9):
                 if board[i][j] != '.':
-                    rows[i][board[i][j]] += 1
-                    cols[j][board[i][j]] += 1
-                    
-        # add the numbers to their respective box           
-        for  i in [0, 3, 6]:
-            for m in [0, 3, 6]:
-                for j in range(i, i + 3):
-                    for k in range(m, m + 3):
-                        box[(j, k)] = (i, m)
-                        if board[j][k] != '.':
-                            boxes[(i, m)][board[j][k]] += 1
-                            
-        is_valid = lambda i, j: rows[i][board[i][j]] <= 1  and cols[j][board[i][j]] <= 1 \
-                    and boxes[box[(i, j)]][board[i][j]] <= 1 
+                    if board[i][j] not in rows[i] and board[i][j] not in cols[j] and board[i][j] not in boxes[(i//3, j//3)]:
+                        rows[i].add(board[i][j])
+                        cols[j].add(board[i][j])
+                        boxes[(i//3, j//3)].add(board[i][j])
+                    else: return False
         
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if not is_valid(i, j):
-                    return False
         return True
 
