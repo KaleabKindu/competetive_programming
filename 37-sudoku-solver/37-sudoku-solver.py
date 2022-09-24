b0f7c5541ed9b12e3376a070a0ea7ee318e7e31b
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-09-24 21:59:01 +0300

    Time: 452 ms (52.43%), Space: 14.1 MB (22.62%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.py
+++ 37-sudoku-solver/37-sudoku-solver.py
@@ -7,8 +7,7 @@ class Solution:
         # holds numbers in each 3x3 boxes
         boxes = defaultdict(set)
         candidates = [str(temp) for temp in range(1, 10)]
-        # holds key value pair of an index to a the corresponding box
-        box = {}
+
         
         # add the numbers to thier respective rows and cols
         for i in range(len(board)):
@@ -22,21 +21,20 @@ class Solution:
             for m in [0, 3, 6]:
                 for j in range(i, i + 3):
                     for k in range(m, m + 3):
-                        box[(j, k)] = (i, m)
                         if board[j][k] != '.':
-                            boxes[(i, m)].add(board[j][k])
+                            boxes[(j//3, k//3)].add(board[j][k])
                             
         is_valid = lambda i, j, val: val not in rows[i] and val not in cols[j] and val not \
-                            in boxes[box[(i, j)]]      
+                            in boxes[(i//3, j//3)]      
         def place_number(row, col, candidate):
             rows[row].add(candidate)
             cols[col].add(candidate)
-            boxes[box[(row, col)]].add(candidate)
+            boxes[(row//3, col//3)].add(candidate)
             board[row][col] = candidate
         def remove_number(row, col, candidate):
             rows[row].remove(candidate)
             cols[col].remove(candidate)
-            boxes[box[(row, col)]].remove(candidate)
+            boxes[(row//3, col//3)].remove(candidate)
             board[row][col] = '.'    
         n = len(board)
         def backtrack():
