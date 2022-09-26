2d0bb972a602d7c1e641d2820c06d44c95dc23f6
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-09-26 04:55:56 +0300

    Time: 84 ms (42.46%), Space: 13.9 MB (94.08%) - LeetHub

---
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.py
@@ -0,0 +1,36 @@
+class UnionFind:
+    def __init__(self):
+        self.root = [i for i in range(26)]
+        
+    def find(self, x):
+        x = ord(x) - 97
+        while self.root[x] != x:
+            x = self.root[x]
+        return x
+        
+    def union(self, x, y):
+        xroot = self.find(x)
+        yroot = self.find(y)
+        if xroot != yroot:
+            self.root[yroot] = xroot
+                
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        UF = UnionFind()
+        
+        for equation in equations:
+            x, operation, y = equation[0], equation[1:3], equation[-1]
+            if operation == '==':
+                UF.union(x, y)
+        
+        for equation in equations:
+            x, operation, y = equation[0], equation[1:3], equation[-1]
+            if operation == '!=':
+                if UF.connected(x, y):
+                    return False
+        return True
+            
+            
+        
