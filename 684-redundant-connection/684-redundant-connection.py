ceeeead0bee284389d48d31b77480cce7ff10f0f
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-09-28 16:10:20 +0300

    Time: 125 ms (24.67%), Space: 14.5 MB (57.14%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.py
@@ -0,0 +1,38 @@
+class UnionFind:
+    def __init__(self, n):
+        self.root = [i for i in range(n + 1)]
+        self.rank = [0 for i in range(n + 1)]
+    def find(self,node1):
+        if self.root[node1] == node1:
+            return node1
+        self.root[node1] = self.find(self.root[node1])
+        return self.root[node1]
+    def union(self,node1, node2):
+        root1 = self.find(node1)
+        root2 = self.find(node2)
+        if root1 != root2:
+            if self.rank[root1] > self.rank[root2]:
+                self.root[root2] = root1
+            elif self.rank[root1] < self.rank[root2]:
+                self.root[root1] = root2
+            else:
+                self.root[root2] = root1
+                self.rank[root1] += 1
+                
+    def connected(self, node1, node2):
+        return self.find(node1) == self.find(node2)
+        
+        
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        Uf = UnionFind(n)
+        redundant = []
+        
+        for head, tail in edges:
+            if not Uf.connected(head, tail):
+                Uf.union(head, tail)
+            else:
+                redundant = [head, tail]
+        return redundant
+        
