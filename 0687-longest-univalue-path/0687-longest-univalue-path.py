7c7871dabc027f3e7766cc8e1e30333b951e4818
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-10-10 21:18:08 +0300

    Time: 989 ms (15.24%), Space: 17.8 MB (98.03%) - LeetHub

--- 0687-longest-univalue-path/0687-longest-univalue-path.py
+++ 0687-longest-univalue-path/0687-longest-univalue-path.py
@@ -11,21 +11,15 @@ class Solution:
             if node:
                 left = dfs(node.left)
                 right = dfs(node.right)
-                if left[0] == right[0] == node.val:
-                    self.answer = max(self.answer, left[1] + right[1])
-                    return node.val, max(left[1], right[1]) + 1
-                elif left[0] == node.val:
-                    self.answer = max(self.answer, right[1]-1)
-                    return node.val, left[1] + 1
-                elif right[0] == node.val:
-                    self.answer = max(self.answer, left[1]-1)
-                    return node.val, right[1] + 1
-                else:
-                    self.answer = max(self.answer, left[1] - 1, right[1] - 1)
-                    return node.val, 1
-                    
-            return 1001, 0
-        if root: self.answer= max(dfs()[1]-1,self.answer)
+                left_length = right_length = 0
+                if node.left and node.left.val == node.val:
+                    left_length = left + 1
+                if node.right and node.right.val == node.val:
+                    right_length = right + 1
+                self.answer = max(self.answer, left_length + right_length)
+                return max(left_length, right_length)
+            return 0
+        dfs()
         return self.answer
                 
                 
