9f1eab420346dbe0c5b7a46468dd0f433df527a5
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-10-12 15:52:38 +0300

    Time: 566 ms (7.35%), Space: 14.6 MB (36.51%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -6,28 +6,28 @@ class Solution:
             for v in _graph[u]:
                 graph[u].append(v)
                 graph[v].append(u)
+                
         red = set()
         blue = set()       
         def bipartite_bfs(cur):
-            queue = deque([(cur,'r')])
+            queue = deque([cur])
             red.add(cur)
             while queue:
-                node, color = queue.popleft()
+                node = queue.popleft()
                 for neighbor in graph[node]:
-                    if color == 'r':
+                    if node in red:
                         if neighbor in red:
                             return False
                         if neighbor not in blue:
                             blue.add(neighbor)
-                            queue.append((neighbor, 'b'))
-                    else:
+                            queue.append(neighbor)
+                    elif node in blue:
                         if neighbor in blue:
                             return False
                         if neighbor not in red:
                             red.add(neighbor)
-                            queue.append((neighbor, 'r'))
+                            queue.append(neighbor)
             return True
-        
         for i in range(n):
             if i not in red and i not in blue and not bipartite_bfs(i):
                 return False
