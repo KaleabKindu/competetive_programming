0516c69392bbc0c3818a376b541c23f4832bece4
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-10-14 21:11:50 +0300

    Time: 3459 ms (88.30%), Space: 14 MB (12.47%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,3 +1,4 @@
+
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         w = len(word)
@@ -5,8 +6,16 @@ class Solution:
         directions = [[1,0],[0,1],[-1,0],[0,-1]]
         valid = lambda r, c: 0 <= r < n and 0 <= c < m and board[r][c] == word[self.k]  and not visited[r][c]
         visited = [[False]*m for i in range(n)]
-        
         self.k = 0
+        
+        boardCount = defaultdict(int)
+        for i in range(n):
+            for j in range(m):
+                boardCount[board[i][j]] += 1
+        for char in word:
+            if word.count(char) > boardCount[char]:
+                return False
+        
         def bk(i, j):
             if self.k >= w:
                 return True
@@ -19,7 +28,7 @@ class Solution:
                     self.k -= 1
                     visited[r][c] = False
             return False
-
+    
         for i in range(n):
             for j in range(m):
                 if board[i][j] == word[self.k]:
