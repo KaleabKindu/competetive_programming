334fa30a5932656b15e366f4587713d1e0c33e7a
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-10-16 09:17:48 +0300

    Time: 8208 ms (10.40%), Space: 14.3 MB (5.63%) - LeetHub

--- 0923-3sum-with-multiplicity/0923-3sum-with-multiplicity.py
+++ 0923-3sum-with-multiplicity/0923-3sum-with-multiplicity.py
@@ -2,6 +2,12 @@ class Solution:
     def threeSumMulti(self, arr: List[int], target: int) -> int:
         n = len(arr)
         arr.sort()
+        rightmost = defaultdict(int)
+        leftmost = defaultdict(int)
+        for i in range(n):
+            rightmost[arr[i]] = i
+        for i in range(n - 1, -1, -1):
+            leftmost[arr[i]] = i
         answer = 0
         for i in range(n):
             j = i + 1
@@ -17,19 +23,11 @@ class Solution:
                         m =  k - j + 1
                         answer += factorial(m)//(2 * factorial(m - 2))
                         break
-                    
-                    left = 0
-                    num = arr[j]
-                    while arr[j] == num:
-                        j += 1
-                        left += 1
-
-                    right = 0
-                    num = arr[k]
-                    while arr[k] == num:
-                        k -= 1
-                        right += 1
-
+                    left = rightmost[arr[j]] - j + 1
+                    right = k - leftmost[arr[k]] + 1
+                    j = rightmost[arr[j]] + 1
+                    k = leftmost[arr[k]] - 1
+                
                     answer += left * right
                         
         return answer % (10**9 + 7)
