3f44421b38d1397b9fc9b60254c2747f518636c5
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-10-16 13:23:12 +0300

    Time: 73 ms (25.09%), Space: 13.8 MB (77.24%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -7,39 +7,20 @@ class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-        less = []
-        greater = []
+        answer = left = ListNode()
+        _right = right = ListNode()
         cur = head
         while cur:
             if cur.val >= x:
-                greater.append(cur)
+                right.next = cur
+                right = cur
             else:
-                less.append(cur)
+                left.next = cur
+                left = cur
             cur = cur.next
-        if len(less) >= 2:
-            l, r = 0, 1
-            while r < len(less):
-                less[l].next = less[r]
-                l += 1
-                r += 1
-        if len(greater) >= 2:
-            l, r = 0, 1
-            while r < len(greater):
-                greater[l].next = greater[r]
-                l += 1
-                r += 1
-        answer = None
-        if less and greater:
-            less[-1].next = greater[0]
-            greater[-1].next = None
-            answer = less[0]
-        elif not less:
-            greater[-1].next = None
-            answer = greater[0]
-        elif not greater:
-            less[-1].next = None
-            answer = less[0]
-        return answer
+        left.next = _right.next
+        right.next = None
+        return answer.next
                 
                 
                 
