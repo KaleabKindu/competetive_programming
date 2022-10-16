16451bd675fcc4c1a180b936e429ba5b6053fe63
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-10-16 14:03:18 +0300

    Time: 64 ms (40.76%), Space: 13.9 MB (91.26%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -6,14 +6,12 @@ class Solution:
         n, m = len(board), len(board[0])
         inbound = lambda i, j: 0 <= i < n and 0 <= j < m
         DIR = [[0,1],[1,0],[0,-1],[-1,0],[1,1],[-1,-1],[1,-1],[-1,1]]
-        dies = set()
-        lives = set()
         def neighbors(i, j):
             live_neighbor = 0
             for offset in DIR:
                 r, c = i + offset[0], j + offset[1]
                 if inbound(r, c):
-                    if board[r][c] == 1:
+                    if board[r][c] == 1 or board[r][c] == 2:
                         live_neighbor += 1
             return live_neighbor
         for i in range(n):
@@ -21,18 +19,19 @@ class Solution:
                 if board[i][j] == 1:
                     live_neighbor = neighbors(i, j)
                     if live_neighbor < 2 or live_neighbor > 3:
-                        dies.add((i, j))
+                        board[i][j] = 2
                 else:
                     live_neighbor = neighbors(i, j)
                     if live_neighbor == 3:
-                        lives.add((i, j))
+                        board[i][j] = 3
                         
         for i in range(n):
             for j in range(m):
-                if board[i][j] == 0 and (i, j) in lives:
+                if board[i][j] == 3:
                     board[i][j] = 1
-                elif board[i][j] == 1 and (i, j) in dies:
+                if board[i][j] == 2:
                     board[i][j] = 0
+                
                 
         
         
