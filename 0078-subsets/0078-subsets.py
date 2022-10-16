6f0e063943aba3e3dd5d8dc669b2c8297392b0a3
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-10-16 10:18:00 +0300

    Time: 75 ms (14.73%), Space: 14.1 MB (35.71%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        answer = []
-        for mask in range(2**n):
-            subset = []
-            for i in range(n):
-                if mask & (1 << i):
-                    subset.append(nums[i])
-            answer.append(subset[:])
-            
+        answer = set()
+        path = []
+        def bk(i = 0):
+            if i >= n:
+                answer.add(tuple(path))
+                return
+            answer.add(tuple(path))
+            bk(i + 1)
+            path.append(nums[i])
+            bk(i + 1)
+            path.pop()
+        bk()
         return answer
