42d2f284e3b5d7dc81542c7e79daf43fdad5c5e2
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-10-17 16:45:39 +0300

    Time: 54 ms (49.25%), Space: 14 MB (13.40%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        
+        @cache
+        def dp(i = 1, j = n):
+            if i >= j:
+                return 1 
+            subtrees = 0
+            for k in range(i, j + 1):
+                subtrees += dp(i, k - 1) * dp(k + 1, j)
+            return subtrees 
+        
+        return dp() 
+    
+                
+            
+        
+        
+        
+        
+        
+        
+        
+        
+
+                
+                
