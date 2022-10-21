f9f620e73870e55b48d309cbd4e3bab726ac7eb2
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-10-21 22:18:27 +0300

    Time: 120 ms (34.68%), Space: 13.8 MB (80.13%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,32 +1,38 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
+        
         self.answer = 0
         
-        
         cols = set()
-        pdiagonals = set()
-        ndiagonals = set()
         
-        is_not_under_attack = lambda row, col: col not in cols and row - col not in \
-                            pdiagonals and (row + col) not in ndiagonals
+        pdiagonal = set()
+        
+        ndiagonal = set()
+        
+        is_not_under_attack = lambda r, c: c not in cols and r - c not in pdiagonal and r + c not in ndiagonal
         
         def place_queen(row, col):
             cols.add(col)
-            pdiagonals.add(row - col)
-            ndiagonals.add(row + col)
+            pdiagonal.add(row - col)
+            ndiagonal.add(row + col)
         def remove_queen(row, col):
             cols.remove(col)
-            pdiagonals.remove(row - col)
-            ndiagonals.remove(row + col)    
+            pdiagonal.remove(row - col)
+            ndiagonal.remove(row + col)    
+        
         
         def backtrack(row = 0):
             if row >= n:
                 self.answer += 1
+                return 
             for col in range(n):
                 if is_not_under_attack(row, col):
-                    place_queen(row, col)
+                    place_queen(row,col)
                     backtrack(row + 1)
                     remove_queen(row, col)
-        
+                    
         backtrack()
         return self.answer
+                    
+        
+        
