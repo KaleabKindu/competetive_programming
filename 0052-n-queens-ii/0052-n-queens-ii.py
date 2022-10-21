e42622c1aebbb47243f57e32be45c76bda4f4b78
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-10-21 20:38:07 +0300

    Time: 125 ms (33.24%), Space: 14 MB (37.13%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -2,7 +2,6 @@ class Solution:
     def totalNQueens(self, n: int) -> int:
         self.answer = 0
         
-        placement = [['.' for i in range(n)] for i in range(n)]
         
         cols = set()
         pdiagonals = set()
@@ -12,12 +11,10 @@ class Solution:
                             pdiagonals and (row + col) not in ndiagonals
         
         def place_queen(row, col):
-            placement[row][col] = 'Q'
             cols.add(col)
             pdiagonals.add(row - col)
             ndiagonals.add(row + col)
         def remove_queen(row, col):
-            placement[row][col] = '.'
             cols.remove(col)
             pdiagonals.remove(row - col)
             ndiagonals.remove(row + col)    
