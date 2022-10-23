80f144bcf421af91d4927a92ba688c345fea2d9a
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-10-23 10:48:12 +0300

    Time: 168 ms (62.30%), Space: 17.8 MB (32.75%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -1,35 +1,14 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.array = nums
-        self.n = len(nums) - 1
-        self.tree = [0 for i in range(4*len(nums))]
-        self.build(0, 0, self.n)
-        
-    def build(self,node, l, r):
-        if l == r:
-            self.tree[node] = self.array[l]
-        else:
-            mid = (l + r)//2
-
-            self.build(2*node + 1, l, mid)
-            self.build(2*node + 2, mid + 1, r)
-
-            self.tree[node] = self.tree[2*node + 1] + self.tree[2*node + 2]
-        
-
-    def sumRange(self, left: int, right: int) -> int:
-        def query(node = 0, l = 0, r = self.n):
-            if right < l or left > r:
-                return 0
-            if left <= l and r <= right:
-                return self.tree[node]
-            mid = (l + r)//2
-            _left = query(2*node + 1, l, mid)
-            _right = query(2*node + 2, mid + 1, r)
+        self.prefix = nums
+        for i in range(1, len(nums)):
+            self.prefix[i] += self.prefix[i - 1]
             
-            return _left + _right
-        return query()
+    def sumRange(self, left: int, right: int) -> int:
+
+        return self.prefix[right] if not left else self.prefix[right] - self.prefix[left - 1]
+        
         
 
 
