9e44553a45a43201c483271aa0007165ceea6f7a
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-10-30 14:43:34 +0300

    Time: 5146 ms (73.44%), Space: 48.8 MB (16.39%) - LeetHub

---
+++ 2290-minimum-obstacle-removal-to-reach-corner/2290-minimum-obstacle-removal-to-reach-corner.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minimumObstacles(self, grid: List[List[int]]) -> int:
+        n, m = len(grid), len(grid[0])
+        
+        directions = [[0,1],[1,0],[0,-1],[-1,0]]
+        in_bound = lambda r, c: 0 <= r < n and 0 <= c < m
+        
+        
+        heap = [(0, 0, 0)] # row, col, obstacles
+        heapify(heap)
+        visited = defaultdict(lambda :float("inf"))
+        
+        while heap:
+            obs, r, c = heappop(heap)
+            if r == n - 1 and c == m - 1:
+                return obs
+            
+            for direction in directions:
+                nr, nc = r + direction[0], c + direction[1]
+                if in_bound(nr, nc):
+                    new_val = obs + grid[nr][nc]
+                    if new_val < visited[(nr,nc)]:
+                        visited[(nr, nc)] = new_val
+                        heappush(heap, (new_val, nr, nc))
+
+        
+        
