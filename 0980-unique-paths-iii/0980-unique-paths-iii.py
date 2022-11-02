cd4f0f200cc9ce4b949c9c5df5791e9a39a4575a
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-11-02 16:07:59 +0300

    Time: 66 ms (87.02%), Space: 13.9 MB (94.95%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -9,25 +9,24 @@ class Solution:
                 if grid[i][j] == 0:
                     self.emptycells += 1
         visited = [[False]*m for i in range(n)]
-        def dp(i, j, empty = 0):
+        def walk(i, j, empty = 0):
             if grid[i][j] == -1:
                 return 0
             if grid[i][j] == 2:
                 return 1 if empty == self.emptycells else 0
+            visited[i][j] = True
             paths = 0
             for direction in directions:
                 r, c = i + direction[0], j + direction[1]
                 if in_bound(r, c) and not visited[r][c]:
-                    visited[r][c] = True
-                    paths += dp(r, c, empty + 1 if grid[r][c] == 0 else empty)
-                    visited[r][c] = False
+                    paths += walk(r, c, empty + 1 if grid[r][c] == 0 else empty)
+            visited[i][j] = False
             return paths
         
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 1:
-                    visited[i][j] = True
-                    return dp(i, j)
+                    return walk(i, j)
             
             
             
