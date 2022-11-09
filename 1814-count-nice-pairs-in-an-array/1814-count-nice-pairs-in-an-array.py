7c52084d4757ec2737879c562ccbf401ea6296f3
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2022-11-09 03:55:27 +0300

    Time: 3080 ms (5.09%), Space: 24.6 MB (15.10%) - LeetHub

---
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countNicePairs(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return  0
+        n = len(nums)
+        def reverse_number(num):
+            rev_num = 0
+            while num:
+                digit = num % 10
+                num = num // 10
+                rev_num = rev_num*10 + digit
+            return rev_num
+        
+        unique = defaultdict(int)
+        for i in range(n):
+            reverse = reverse_number(nums[i])
+            unique[nums[i] - reverse] += 1
+        total = sum(unique.values())
+        nice = 0
+        for val in unique.values():
+            if val >= 2:
+                nice += (factorial(val) // (2 * factorial(val - 2)))
+            
+        return nice % (10**9 + 7)
