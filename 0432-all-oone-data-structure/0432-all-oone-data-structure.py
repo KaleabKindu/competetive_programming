6477866987540196f6bfbfd2eb6d7070f6a58fc6
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2023-02-13 12:35:02 -0800

    Time: 221 ms (64.06%), Space: 34.5 MB (5.63%) - LeetHub

---
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.py
@@ -0,0 +1,43 @@
+class AllOne:
+
+    def __init__(self):
+        
+        self.count=defaultdict(int)
+        self.min=[]
+        self.max=[]
+        
+
+    def inc(self, key: str) -> None:
+  
+        self.count[key]+=1
+        heappush(self.min, [self.count[key], key])
+        heappush(self.max, [-self.count[key], key])
+
+    def dec(self, key: str) -> None:
+        self.count[key]-=1
+        heappush(self.min, [self.count[key], key])
+        heappush(self.max, [-self.count[key], key])
+
+    def getMaxKey(self) -> str:
+        while self.max:
+            most, value = self.max[0]
+            if most == 0:
+                heappop(self.max)
+                continue
+            if self.count[value]==-most:
+                return value
+            heappop(self.max)
+        return ''
+
+    def getMinKey(self) -> str:
+        while self.min:
+            most, value=self.min[0]
+            if most==0:
+                heappop(self.min)
+                continue
+                
+            if self.count[value]==most:
+                return value
+            heappop(self.min)
+        return ''
+        
