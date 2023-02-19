d32cea6036edaffbad0e5c51533e4a8c7e64b826
Kaleab Kindu <87898472+kkstarkk@users.noreply.github.com>
2023-02-18 22:53:04 -0800

    Time: 122 ms (57.88%), Space: 15.5 MB (16.44%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        nums.sort()
+        answer = []
+        
+        i = 0
+        while i < n:
+            j = i
+            while j < n and nums[i] == nums[j]:
+                j += 1
+            if j - i > n//3:
+                answer.append(nums[i])
+            i = j
+            
+        return answer
+            
